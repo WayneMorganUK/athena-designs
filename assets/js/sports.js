f912ae0335e13901e3a42f5ead6ef5386df94921
WormCrew <44246461+WormCrew@users.noreply.github.com>
2019-01-30 21:08:36 +0000

    Titles added to sports.js

--- assets/js/sports.js
+++ assets/js/sports.js
@@ -1,7 +1,6 @@
 var sourceUrl = [
   "https://barrystantonofficial.azureedge.net/0f2453cc-8aa9-4a2d-87f8-3e1cb4ea7b0f/9d699859-0eef-4999-aa4b-8e30c79abf3c.ism/manifest(video,format=m3u8-aapl-v3,audiotrack=audio).m3u8",
 
-
   "https://fricmoddedsstrim.azureedge.net/e88d119a-84e3-4885-ac99-482fbefac399/7e2f2ab3-cff6-4309-8100-ad86909c50b0.ism/manifest(video,format=m3u8-aapl-v3,audiotrack=audio).m3u8",
 
   "https://fricmoddedsstrim.azureedge.net/e88d119a-84e3-4885-ac99-482fbefac399/7e2f2ab3-cff6-4309-8100-ad86909c50b0.ism/manifest(video,format=m3u8-aapl-v3,audiotrack=audio).m3u8",
@@ -16,18 +15,18 @@ var title = [
   "Liverpool v Leicester City",
   "Liverpool v Leicester City",
   "Liverpool v Leicester City",
-  "Liverpool v Leicester City",
-  "Liverpool v Leicester City",
-  "Liverpool v Leicester City"
+  "Bournemouth v Chelsea",
+  "tbc",
+  "tbc"
 ];
 
 var player = []
 for (var i = 0; i < 6; i++) {
+  document.getElementById("player" + (i+1)).innerHTML = title[i];
   player[i] = new Clappr.Player({
   source: sourceUrl[i],
   width: '384px',
   height: '216px',
   parentId: "#player" + (i+1)
-  });
-  document.getElementById("#player" + (i+1)).textContent = title[i];
+  })
 }
