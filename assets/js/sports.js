c7c7d8ba5ac73219f57183ac21276a5325612ff3
WormCrew <44246461+WormCrew@users.noreply.github.com>
2019-02-03 18:18:05 +0000

    Update sports.js

--- assets/js/sports.js
+++ assets/js/sports.js
@@ -1,13 +1,13 @@
 var sourceUrl = [
-  "http://roedflvoerl.azureedge.net/w/playlist.m3u8",
+  "https://waynerooneystream.azureedge.net/7370e1ca-6b4f-4623-93cb-56f6b7e11d52/3e131bec-8f5f-49f1-b6b3-cfeec4360e72.ism/manifest(video,format=m3u8-aapl-v3,audiotrack=audio).m3u8",
 
   "https://baldystreamer.azureedge.net/83fe2f5a-1b09-4202-b200-56f351a07fb5/92c2a94c-ff11-409e-a439-ca1b12c2bc42.ism/manifest(video,format=m3u8-aapl-v3,audiotrack=audio).m3u8",
 
-  "http://sidovlerovle.azureedge.net/l/playlist.m3u8",
+  "http://roedflvoerl.azureedge.net/w/playlist.m3u8",
 
-  "https://barrystantonofficial.azureedge.net/5307bef2-3e9b-4655-8887-7648107b86ef/b6e0a94c-d1c1-4653-8507-ca578a140754.ism/manifest(video,format=m3u8-aapl-v3,audiotrack=audio).m3u8",
+  "http://shacktv.com:8080/live/alessandro01/alessandro01/21797.m3u8",
 
-  "https://hls305304.r-cdn.com/trn_61810/561/playlist.m3u8?st=mR2XE9TY52uOhnHk364SLQ&e=1547999424",
+  "http://192.240.113.138:8080/live/alessandro01/alessandro01/21797.m3u8",
 
   "http://whostreams.net/87546f8d-fcfd-4ea5-be7e-e672476813d7.ism/manifest(video,format=m3u8-aapl-v3,audiotrack=audio).m3u8"
 ]
